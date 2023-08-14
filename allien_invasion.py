b55c04e7bd4defb90509c7d15ee51b530e60307d
Enimalojd <enderston8@gmail.com>
2023-08-14 16:24:10 +0400

    Загрузка изображения корабля + отображение его на экране

--- allien_invasion.py
+++ allien_invasion.py
@@ -1,15 +1,17 @@
 import sys
 import pygame
-
+from setting import Settings
+from ship import Ship
 
 class AlienInvasion:
     # Класс для управления ресурсами и поведением игры
     def __init__(self):
         # инициализирует игру и создаёт игровые ресусры
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_wight, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (65, 74, 90)
+        self.ship = Ship(self.screen)
 
     def run_game(self):
         # Запуск основного цикла игры
@@ -19,11 +21,12 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 
 
 if __name__ == "__main__":
-    # создание эжкземпляра и запуск игры
+    # создание экземпляра и запуск игры
     ai = AlienInvasion()
     ai.run_game()
