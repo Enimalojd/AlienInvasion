0fa58898602cd58bc1ec50674b88a62f4a62d785
Enimalojd <enderston8@gmail.com>
2023-08-15 15:43:47 +0400

    Фуллскрин+Q для выхода

--- setting.py
+++ setting.py
@@ -1,6 +1,6 @@
 class Settings():
     def __init__(self):
-        self.screen_wight = 1200
+        self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (65, 74, 90)
-        self.ship_speed = 1.1
+        self.ship_speed = 0.3
--- ship.py
+++ ship.py
@@ -7,6 +7,7 @@ class Ship:
         # инициализирует корабль и задаёт его начальную форму
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # загружаем картинку корабля
@@ -14,15 +15,19 @@ class Ship:
         self.rect = self.image.get_rect()
         # каждый новый корабль появляется снизу по центру
         self.rect.midbottom = self.screen_rect.midbottom
+        #сохранение вещественной координаты центра коробля
+        self.x = float(self.rect.x)
         #флажки для перемещения
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        self.rect.x =self.x
 
     def blitme(self):
         # Рисует корабль в текущей позиции
