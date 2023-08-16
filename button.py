f58dc7705c3cb53f3d0c1e969a33821a98205a5a
Enimalojd <enderston8@gmail.com>
2023-08-16 17:00:35 +0400

    Добавил класс будущей кнопки Play

---
+++ button.py
@@ -0,0 +1,28 @@
+import pygame.font
+
+class Button():
+    def __init__(self, ai_game, msg):
+        #инициализирует атрибуты и кнопки
+        self.screen = ai_game.screen
+        self.screen.rect = self.screen.rect.get_rect()
+        #назначение размеров и свойст кнопок
+        self.width, self.height = 200, 50
+        self.button_color = (0, 255, 0)
+        self.tex_color = (0, 0, 0)
+        self.font = pygame.font.SysFont(None, 48)
+        #построение кнопки и выравнивание по центру экрана
+        self.rect = pygame.Rect(0, 0, self.width, self.height)
+        self.rect.center = self.screen_rect.center
+        #сообщение кнопки создаётся один раз
+        self._prep_msg(msg)
+
+    def _prep_msg(self, msg):
+        #преобразует msg в прямоугольник и выравнивает текст по центру
+        self.msg_image = self.font.render(msg, True, self.tex_color, self.button_color)
+        self.msg_image_rect = self.msg_image.get_rect()
+        self.msg_image_rect.center = self.rect.center
+
+    def draw_button(self)
+        #отображение пустой кнопки и вывод сообщения
+        self.screen.fill(self.button_color, self.rect)
+        self.screen.blit(self.msg_image, self.msg_image_rect)
--- game_stats.py
+++ game_stats.py
@@ -5,7 +5,7 @@ class GameStats():
         #инициализирует статистику
         self.settings = ai_game.settings
         self.reset_stats()
-        self.game_active = True
+        self.game_active = False
 
     def reset_stats(self):
         #Инициализирует статистику, которая меняется в ходе игры
